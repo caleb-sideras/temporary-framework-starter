5310e675e9c690f54580c29a52402762faebcacd
Caleb Sideras <caleb.sideras@outlook.com>
2024-01-17 19:45:59 -0500

    added text content

--- src/app/projects/page.go
+++ src/app/projects/page.go
@@ -23,24 +23,28 @@ func Page(w http.ResponseWriter, r *http.Request) templ.Component {
 			Title:       "GoX Framework",
 			Description: "A framework designed to make working with Go and HTMX easier. Achieves this by employing certain primitives that tightly couple them, aiding scaling.",
 			Src:         "https://gox-framework.org/static/assets/gox-mascot-hor.png",
+			Href:        "/projects/goxFramework",
 			Alt:         "GoX Framework Moscot",
 		},
 		{
 			Title:       "CAAS",
 			Description: "CAAS is an audio segmentation algorithm that iteratively finds the best intervals and patterns in a given audio signal, clusters them based on similarity and then extracts relevant audio features (Lyrics, MIDI etc).",
 			Src:         "https://opengraph.githubassets.com/1a43d7c7f54d054a1094b6f53a76668f5d192eafd3f041c978cce36355cf3006/caleb-sideras/CAAS",
+			Href:        "/projects/caas",
 			Alt:         "CAAS Github",
 		},
 		{
 			Title:       "People Pedia",
 			Description: "AI powered search that summarizes and visualizes information for anyone who has data on the internet.",
 			Src:         "https://opengraph.githubassets.com/be1d1b6657697642586856c35f9916242a1108d34d8cee509b61c2ca5b5e2e56/caleb-sideras/PeoplePedia-Backend",
+			Href:        "/projects/peoplepedia",
 			Alt:         "People Pedia Github",
 		},
 		{
 			Title:       "Tweetailyze",
 			Description: "A web app that performs Twitter account summarization through tweet embedding, clustering, sentiment analysis and topic modeling.",
 			Src:         "https://opengraph.githubassets.com/bc52bfa035af32496e45f674130251555d2549ca38d8b333461df836979cebbb/caleb-sideras/tweetailyze-frontend",
+			Href:        "/projects/tweetailyze",
 			Alt:         "Tweetailyze Github",
 		},
 		{
