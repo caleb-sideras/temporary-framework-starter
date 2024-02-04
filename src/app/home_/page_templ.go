e25756906a2d3c60bcd6bc9329ebcc9519231d60
Caleb Sideras <caleb.sideras@outlook.com>
2024-02-04 14:39:36 -0500

    hard af homescreen

--- src/app/home_/page_templ.go
+++ src/app/home_/page_templ.go
@@ -23,43 +23,7 @@ func HomeTempl() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"home_container\"><div class=\"home_flexbox\"><div class=\"home_left\"><h1 class=\"home_heading\">")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Var2 := `hello there`
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var2)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</h1><div class=\"home_ipa\">")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Var3 := `IPA   /ˈkeɪləb/ — 'dog-like' (he got that dawg in him)`
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var3)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div></div><div class=\"home_right\"><img class=\"home_image\" loading=\"lazy\" src=\"/static/assets/caleb_sideras_1.jpeg\" alt=\"Caleb James Sideras Face\"></div></div><div class=\"home_bio\">")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Var4 := `I'm Caleb - a vim-pilled, based, terminal-enjoyer junior software developer. Over the years i've tried various abstractions - React over the DOM, Javascript over C++, Meta-frameworks over frameworks over libraries over languages over other languages over assembly over machine code over math over physics over reality. Anyways, the point i'm trying to make is that i'm on a journey to understand on an low-level, all the tools i'm using. Not quite there yet but i'm trying. I love Go, HTMX, Typescript (because i have to) and life.    `
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var4)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div><div class=\"home_disclaimer\">")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Var5 := `**disclaimer - this website is running on the alpha build of my own framework and ui library. there may be bugs!!  `
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var5)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div></div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<img class=\"home_image\" loading=\"lazy\" src=\"/static/assets/temporary.png\" alt=\"Caleb James Sideras Face\"> <img class=\"home_image\" loading=\"lazy\" src=\"/static/assets/temporary-2.png\" alt=\"Caleb James Sideras Face\"> <img class=\"home_image\" loading=\"lazy\" src=\"/static/assets/temporary-3.png\" alt=\"Caleb James Sideras Face\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
